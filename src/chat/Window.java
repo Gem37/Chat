2d2631ab7cde759bb60604be64a6d3bd68a58523
George Krikorian <georgekrikorian@georges-mbp.lan>
2019-07-06 10:01:02 -0700

    Select Users

--- src/chat/Window.java
+++ src/chat/Window.java
@@ -1,13 +1,21 @@
 package chat;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.BoxLayout;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,13 +23,15 @@ import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTextField;
 
-public class Window extends JPanel implements ActionListener {
-	JFrame frame = new JFrame();
+public class Window extends JPanel implements ActionListener, MouseListener {
+	static JFrame frame = new JFrame();
 	JTextField input;
 	JLabel usernamePrompt;
 	static JPanel usernameList = new JPanel();
 	JPanel inputPanel = new JPanel();
-	JSplitPane splitPane;
+	static JSplitPane splitPane;
+	static String username;
+	static String selectedUser;
 	
 	public Window() {
 		frame.setPreferredSize(new Dimension(600, 400));
@@ -35,11 +45,15 @@ public class Window extends JPanel implements ActionListener {
 		input.addActionListener(this);
 		inputPanel.add(usernamePrompt);
 		inputPanel.add(input);
+		usernameList.addMouseListener(this);
 		usernameList.setLayout(new BoxLayout(usernameList, BoxLayout.Y_AXIS));
+		
 		
 		usernameList.removeAll();
 		for(String user : Networking.usersOnline) {
-			usernameList.add(new JLabel(user));
+			JLabel userLabel = new JLabel(user);
+			
+			usernameList.add(userLabel);
 		}
 		
 		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, usernameList, inputPanel);
@@ -63,34 +77,77 @@ public class Window extends JPanel implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == input && Main.hasUsername) {
-			
-			Main.networking.sendMessage(input.getText());
+		System.out.println("We Recieved an Event");
+		if(e.getSource() == input) {
+			if(Main.hasUsername) {
+				Main.networking.sendMessage(input.getText());
 			
 			System.out.println("Sent Message: " + input.getText());
 			
 			input.setText("");
 			
-		} else if(!Main.hasUsername) {
+			} else {
+				
+				username = input.getText();
+				
+				Main.networking.sendMessage(username);
+				System.out.println("Username Set To: " + username);
+				
+				input.setText("");
+				
+				Main.hasUsername = true;
+				inputPanel.remove(usernamePrompt);
+				
+				frame.repaint();
+			}
 			
-			Main.networking.sendMessage(input.getText());
-			
-			System.out.println("Username Set To: " + input.getText());
-			
-			input.setText("");
-			
-			Main.hasUsername = true;
-			inputPanel.remove(usernamePrompt);
-			
-			frame.repaint();
 		}
+	}
+	
+	public static boolean isMouseWithinComponent(Component c) {
+	    Point mousePos = MouseInfo.getPointerInfo().getLocation();
+	    Rectangle bounds = c.getBounds();
+	    bounds.setLocation(c.getLocationOnScreen());
+	    return bounds.contains(mousePos);
 	}
 	
 	public static void updateList() {
 		usernameList.removeAll();
 		for(String user : Networking.usersOnline) {
-			usernameList.add(new JLabel(user));
+			if(!user.equals(username)) {
+				usernameList.add(new JLabel(user));
+			}
 		}
+		frame.pack();
+		splitPane.resetToPreferredSizes();
 		usernameList.repaint();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if(usernameList.findComponentAt(e.getPoint()) instanceof JLabel) {
+			selectedUser = ((JLabel) usernameList.findComponentAt(e.getPoint())).getText();
+			System.out.println(selectedUser);
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		
 	}
 }
